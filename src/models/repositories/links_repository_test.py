f287dd5af06eaeb9b286326214023e27e40e7981
gusrberto <gustavorsdemelo@gmail.com>
2024-07-12 21:30:29 -0300

    finishing implementing links model

---
+++ src/models/repositories/links_repository_test.py
@@ -0,0 +1,32 @@
+import pytest
+import uuid
+from .links_repository import LinksRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
+link_id = str(uuid.uuid4())
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_register_link():
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+
+    link_infos = {
+        "id": link_id,
+        "trip_id": trip_id,
+        "link": "somelink.cim",
+        "title": "Hotel"
+    }
+
+    link_repository.register_link(link_infos)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_find_links_from_trip():
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+
+    response = link_repository.find_links_from_trip(trip_id)
+
+    assert isinstance(response, list)
+    assert isinstance(response[0], tuple)
